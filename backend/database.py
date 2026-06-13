3864207c241946f77568ddf6ca4431022629662d
Luciano Salinas Tapia <luspacexx@gmail.com>
2026-06-13 16:03:13 -0400

    feat(database): agregar async database con SQLAlchemy y dotenv

---
+++ backend/database.py
@@ -0,0 +1,27 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL no está configurada correctamente")
+
+engine = create_async_engine(DATABASE_URL, echo=True) # echo=True muestra cada query SQL en consola.
+
+ASYNC_SESSION_LOCAL = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+class Base(DeclarativeBase):
+    "Clase Base para"
+    pass
+
+async def get_db():
+    async with ASYNC_SESSION_LOCAL() as session:
+        yield session
+
